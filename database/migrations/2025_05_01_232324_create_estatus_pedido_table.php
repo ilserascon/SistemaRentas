ff047fc1cf72d5afc352b977641a86203e5f0d2f
Ilse Rascón <ilserascon@gmail.com>
2025-05-01 16:57:21 -0700

    creacion de tablas
    tablas base para el sistema de rentas

---
+++ database/migrations/2025_05_01_232324_create_estatus_pedido_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('estatus_pedido', function (Blueprint $table) {
+            $table->id();
+            $table->string('descripcion');
+            $table->timestamps();
+        });
+
+        DB::table('estatus_pedido')->insert([
+            ['descripcion' => 'Solicitado'],
+            ['descripcion' => 'En Entrega'],
+            ['descripcion' => 'En Renta'],
+            ['descripcion' => 'Terminado'],
+            ['descripcion' => 'Cancelado'],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('estatus_pedido');
+    }
+};
